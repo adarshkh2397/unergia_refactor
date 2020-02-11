43269928ca9895afe1dc21cafe37d34440ab3335
Adarsh <adarshkhandelwal.work@gmail.com>
2020-02-11 14:07:32 +0530

    correct mobile view of how unergia works

---
+++ js/custom.js
@@ -0,0 +1,89 @@
+$('#sch').on('click' , () => {
+    $('.show1').removeClass('d-none');
+    $('.show2').addClass('d-none');
+    $('.show3').addClass('d-none');
+    $('.show4').addClass('d-none');
+    $('.show5').addClass('d-none');
+    $('.show6').addClass('d-none');
+    $('.show7').addClass('d-none');
+    $('.show8').addClass('d-none');
+})
+
+$('#p1').on('click' , () => {
+    $('.show1').addClass('d-none');
+    $('.show2').removeClass('d-none');
+    $('.show3').addClass('d-none');
+    $('.show4').addClass('d-none');
+    $('.show5').addClass('d-none');
+    $('.show6').addClass('d-none');
+    $('.show7').addClass('d-none');
+    $('.show8').addClass('d-none');
+})
+
+$('#p2').on('click' , () => {
+    $('.show1').addClass('d-none');
+    $('.show2').addClass('d-none');
+    $('.show3').removeClass('d-none');
+    $('.show4').addClass('d-none');
+    $('.show5').addClass('d-none');
+    $('.show6').addClass('d-none');
+    $('.show7').addClass('d-none');
+    $('.show8').addClass('d-none');
+})
+
+$('#p3').on('click' , () => {
+    $('.show1').addClass('d-none');
+    $('.show2').addClass('d-none');
+    $('.show3').addClass('d-none');
+    $('.show4').removeClass('d-none');
+    $('.show5').addClass('d-none');
+    $('.show6').addClass('d-none');
+    $('.show7').addClass('d-none');
+    $('.show8').addClass('d-none');
+})
+
+$('#msg').on('click' , () => {
+    $('.show1').addClass('d-none');
+    $('.show2').addClass('d-none');
+    $('.show3').addClass('d-none');
+    $('.show4').addClass('d-none');
+    $('.show5').removeClass('d-none');
+    $('.show6').addClass('d-none');
+    $('.show7').addClass('d-none');
+    $('.show8').addClass('d-none');
+})
+
+$('#p4').on('click' , () => {
+    $('.show1').addClass('d-none');
+    $('.show2').addClass('d-none');
+    $('.show3').addClass('d-none');
+    $('.show4').addClass('d-none');
+    $('.show5').addClass('d-none');
+    $('.show6').removeClass('d-none');
+    $('.show7').addClass('d-none');
+    $('.show8').addClass('d-none');
+})
+
+$('#p5').on('click' , () => {
+    $('.show1').addClass('d-none');
+    $('.show2').addClass('d-none');
+    $('.show3').addClass('d-none');
+    $('.show4').addClass('d-none');
+    $('.show5').addClass('d-none');
+    $('.show6').addClass('d-none');
+    $('.show7').removeClass('d-none');
+    $('.show8').addClass('d-none');
+})
+
+$('#p6').on('click' , () => {
+    $('.show1').addClass('d-none');
+    $('.show2').addClass('d-none');
+    $('.show3').addClass('d-none');
+    $('.show4').addClass('d-none');
+    $('.show5').addClass('d-none');
+    $('.show6').addClass('d-none');
+    $('.show7').addClass('d-none');
+    $('.show8').removeClass('d-none');
+})
+
+
